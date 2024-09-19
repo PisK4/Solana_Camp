93f62ebefabd357921dbe7881e4dd3232e2530b2
0x16whale <ouyangmin125@gmail.com>
2024-09-19 15:43:15 +0800

    update uint256_lib=>check_div

--- programs/vizing_core/src/library/uint256_lib.rs
+++ programs/vizing_core/src/library/uint256_lib.rs
@@ -11,12 +11,6 @@ impl Uint256 {
         Self { high, low }
     }
 
-    pub fn add(self, other: Self) -> Self {
-        let (low, carry) = self.low.overflowing_add(other.low);
-        let high = self.high + other.high + if carry { 1 } else { 0 };
-        Self { high, low }
-    }
-
     pub fn check_add(self, other: Self) -> Option<Self> {
         let (low, carry) = self.low.overflowing_add(other.low);
         let high = self
@@ -52,17 +46,66 @@ impl Uint256 {
 
     pub fn check_div(self, other: Self) -> Option<Self> {
         if other.is_zero() {
-            return None; 
+            msg!("Division by zero error!");
+            return None;
         }
-        let low = self.low.checked_div(other.low)?;
-        let high = if self.high == 0 {
-            0 
-        } else {
-            let total_self = (self.high.checked_shl(128))? + self.low; 
-            let total_other = (other.high.checked_shl(128))? + other.low; 
-            total_self.checked_div(total_other)? 
-        };
 
+        let (high, low): (u128, u128);
+
+        if self.high == 0 && other.high == 0 {
+            high = 0;
+            low = self.low.checked_div(other.low)?;
+        } else if self.high == 0 && other.high != 0 {
+            high = 0;
+            low = 0;
+        } else if self.high != 0 && other.high == 0 {
+            if other.low == 0 {
+                high = 0;
+                low = 0;
+            } else {
+                let a = u128::MAX.checked_div(other.low)?;
+                let new_number1 = Uint256::new(0, a);
+                let new_number2 = Uint256::new(0, self.high);
+                let final_high_div_num = new_number1.check_mul(new_number2)?;
+                let final_low_div;
+                if self.low != 0 {
+                    final_low_div = self.low.checked_div(other.low)?;
+                } else {
+                    final_low_div = 0;
+                }
+                let final_low_div_num = Uint256::new(0, final_low_div);
+                let final_uint256 = final_high_div_num.check_add(final_low_div_num)?;
+                high = final_uint256.high;
+                low = final_uint256.low;
+            }
+        } else {
+            let (double_other_high, over_mul) = other.high.overflowing_mul(2);
+            if self.high > other.high {
+                if over_mul {
+                    high = 0;
+                    low = 1;
+                } else {
+                    if self.high > double_other_high {
+                        high = 0;
+                        low = self.high.checked_div(other.high)?;
+                    } else {
+                        if self.high == double_other_high && other.low == 0 {
+                            high = 0;
+                            low = 2;
+                        } else {
+                            high = 0;
+                            low = 1;
+                        }
+                    }
+                }
+            } else if self.high == other.high && self.low >= other.low {
+                high = 0;
+                low = 1;
+            } else {
+                high = 0;
+                low = 0;
+            }
+        }
         Some(Self { high, low })
     }
 
