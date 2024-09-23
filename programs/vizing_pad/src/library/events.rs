087603f617a09c382a0c78cd60127962ccf51981
0x16whale <ouyangmin125@gmail.com>
2024-09-23 16:06:54 +0800

    update

--- programs/vizing_pad/src/library/events.rs
+++ programs/vizing_pad/src/library/events.rs
@@ -16,7 +16,7 @@ pub struct SuccessfulLaunchMessage {
     pub sender: Pubkey,
     pub src_contract: Pubkey,
     pub value: Uint256,
-    pub fee: Uint256,
+    pub fee: u64,
     pub dest_chainid: u64,
     pub addition_params: Vec<u8>,
     pub message: Message,
